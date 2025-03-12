ba37001b06b3d60b07f39d1d34b0b7d5ea3e6871
shalomarbsie <151989336+shalomarbsie@users.noreply.github.com>
2025-03-12 17:58:40 +0300

    New functions + program changes
    1. adding of category section to expenses along with an 'add_category' and 'view_categories' function
    2. 'update_expense' now will also update category and time to the time of the update
    3. added a 'summarize_monthly_expense' function to show expenses for requested month
    4. added a 'set_budget' function to set a monthly and also show a warning for certain thresholds reached in the current month
    5. added a 'export_file' to copy the file to the directory of the users choosing
    6. added a 'clear_expenses' function which is just the delete_expenses.py program implemented as a function when the user wants to clear all expenses 
    7. added a variable to set the currency

--- exptrack.py
+++ exptrack.py
@@ -1,24 +1,42 @@
-import argparse
+import shutil
 import sys
 import os
 import csv
-import datetime
+from datetime import datetime
 
+month_to_int = {
+        'january': 1,
+        'february': 2,
+        'march': 3,
+        'april': 4,
+        'may': 5,
+        'june': 6,
+        'july': 7,
+        'august': 8,
+        'september': 9,
+        'october': 10,
+        'november': 11,
+        'december': 12
+    }
 
-file_path = "C:\\Users\\Shalom Arbsie\\projects\\personal\\Expense-Tracker\\expenses.csv"
+currency = None
+file_path = "path\\to\\your\\current\\or\\source\\directory\\expenses.csv"
+destination_path = "path\\to\\your\\export\\destination\\directory\\expensescopy.csv"
 file_name = "expenses.csv"
-current_datetime = datetime.datetime.now()
+current_datetime = datetime.now()
+current_year = current_datetime.year
+current_month = current_datetime.month
 timestamp = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
 
-if not os.path.exists(file_path):
+if not os.path.isfile(file_path):
     expense_headline = [
-        ["Expense Number", "Expense Type", "Expense Amount", "Date and Time"]
+        ["Expense Number", "Expense Type", "Expense Amount", "Date and Time", "Category"]
     ]   
     with open(file_name, "w", newline='') as expenses:
         writer = csv.writer(expenses)
         writer.writerows(expense_headline)
         
-def add_expense(expense_type, expense_amount):
+def add_expense(expense_type, expense_amount, expense_category):
     with open(file_name, 'r', newline='') as expenses:
         reader = csv.reader(expenses)
         rows = list(reader)
@@ -40,7 +58,7 @@ def add_expense(expense_type, expense_amount):
         expense_number = buffer + 1
         rows[-1][0] = str(expense_number)
 
-    rows.append([expense_number, expense_type, expense_amount, timestamp])
+    rows.append([expense_number, expense_type, expense_amount, timestamp, expense_category])
     with open(file_name, "a", newline='') as expenses:
         writer = csv.writer(expenses)
         writer.writerow(rows[-1])
@@ -55,8 +73,11 @@ def update_expense(expense_type):
         if row[1] == expense_type:
             updated_expense = input("enter new expense: ")
             updated_amount = input("enter new amount: ")
+            updated_category = input("enter new category: ")
             row[1] = updated_expense
             row[2] = updated_amount
+            row[4] = updated_category
+            row[3] = timestamp
             found = True
             break
    
@@ -98,7 +119,7 @@ def view_expenses():
     for row in rows:
         print(row)
 
-def summarize_expenses():
+def summarize_total_expenses():
     total_expenses = 0
     with open(file_name, 'r') as expenses:
         reader = csv.reader(expenses)
@@ -111,7 +132,72 @@ def summarize_expenses():
     
     print(f"total expenses: {total_expenses}")
 
-add_expense('dog food', 200)
-add_expense('rent', 1500)
-add_expense('groceries', 700)
-summarize_expenses()
+def summarize_monthly_expenses(month):    
+    int_month = month_to_int.get(month.lower(), None)
+    if int_month is None:
+        print(f"Invalid month: {month}")
+        return
+
+    monthly_expense = 0
+    with open(file_name, 'r') as expenses:
+        reader = csv.reader(expenses)
+        rows = list(reader)
+    
+    for row in rows:
+        if row[3] != "Date and Time":
+            timestamp_dt = datetime.strptime(row[3], "%Y-%m-%d %H:%M:%S")
+            if timestamp_dt.year == current_year:
+                if timestamp_dt.month == int_month:
+                    monthly_expense += int(row[2])
+
+    return monthly_expense
+
+def add_category(expense_category):
+    with open('category.txt', 'a+') as categories:
+        for category in categories:
+            if category == expense_category:
+                print(f"category /{expense_category}/ already exists, please enter another category")
+        
+        categories.write(expense_category)
+
+def view_categories(expense_category):
+    with open(file_name, 'r') as expenses:
+        reader = csv.reader(expenses)
+        rows = list(reader)
+
+    for row in rows:
+        if row[4] == expense_category:
+            print(row)
+    
+def set_budget(budget):
+    for key, val in month_to_int.items():
+        if val == current_month:
+            string_month = key
+    
+    monthly_expense = summarize_monthly_expenses(string_month)
+    leftover_budget = budget - monthly_expense
+    if leftover_budget < 0:
+        print(f"Warning! you have surpassed your budget by {-leftover_budget} {currency}'s for {string_month}: budget leftover = {leftover_budget}")
+    elif leftover_budget < 100:
+        print(f"Warning! you have less than 100 {currency}'s left for {string_month}: budget leftover = {leftover_budget}")
+    elif leftover_budget < 250:
+        print(f"Warning! you have less than 250 {currency}'s left for {string_month}: budget leftover = {leftover_budget}")
+    elif leftover_budget < 500:
+        print(f"Warning! you have less then 500 {currency}'s left for {string_month}: budget leftover = {leftover_budget}")
+    
+    print
+ 
+def export_file():
+    shutil.copy(file_path, destination_path)
+    print(f"{file_name} exported to {destination_path}")
+
+def clear_expenses():
+    try:
+        os.remove(file_name)
+        print(f"File '{file_name}' has been deleted.")
+    except FileNotFoundError:
+        print(f"The file '{file_name}' does not exist.")
+    except PermissionError:
+        print(f"Permission denied to delete the file '{file_name}'.")
+    except Exception as e:
+        print(f"Error deleting file: {e}")
